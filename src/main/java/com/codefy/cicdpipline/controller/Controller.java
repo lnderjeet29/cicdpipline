954299cb18547820c048ac0dc567562ee9cd1067
powerhourseAi_task <inderjeetyadav03022001@gmail.com>
2024-04-06 16:56:14 +0530

    inital commit

---
+++ src/main/java/com/codefy/cicdpipline/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.codefy.cicdpipline.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("get")
+    public ResponseEntity<String> get(){
+        return new ResponseEntity<String>("hello everyone....", HttpStatus.OK);
+    }
+}
---
+++ src/test/java/com/codefy/cicdpipline/CicdpiplineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefy.cicdpipline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdpiplineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
